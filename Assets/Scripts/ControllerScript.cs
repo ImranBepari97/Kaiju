1e220cacae91da46bec9f3182c56c62287559aba
Imran Bepari <imranbepari97@gmail.com>
2018-05-01 18:59:30 +0100

    Initial fracturing works

---
+++ Assets/Scripts/ControllerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerScript : MonoBehaviour {
+
+	public SteamVR_TrackedObject trackedObject;
+	public SteamVR_Controller.Device device; //Device lets you access velocity
+
+	// Use this for initialization
+	void Start () {
+		trackedObject = GetComponent<SteamVR_TrackedObject> ();
+		device = SteamVR_Controller.Input ((int)trackedObject.index);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	//	Debug.Log (device.velocity);
+	}
+}
---
+++ Assets/Scripts/Fracture.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fracture : MonoBehaviour {
+	[SerializeField]
+	GameObject fractureObject;
+
+	[SerializeField]
+	float forceToBreak;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider coll) {
+		if(coll.gameObject.tag == "Player") {
+			
+
+			ControllerScript controller;
+
+			//Check if the force of the impact is big enough to fracture
+			if(controller = coll.gameObject.GetComponent<ControllerScript>()) {
+				
+				Debug.Log ("Collision at " + controller.device.velocity.magnitude);
+				if(Mathf.Abs(controller.device.velocity.magnitude) > forceToBreak) {
+					//Replace?
+					Instantiate (fractureObject, transform.position, Quaternion.identity);
+					Destroy (this.gameObject);
+				}
+			}
+		}
+	}
+}
