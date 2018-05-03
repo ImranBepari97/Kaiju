88864f5f4031d3ce95996245c789296cc3b4c651
Imran Bepari <imranbepari97@gmail.com>
2018-05-03 23:22:08 +0100

    Changed gravity to be 1/6 of 9.81, made blocks fall when not supported

--- Assets/Scripts/Fracture.cs
+++ Assets/Scripts/Fracture.cs
@@ -9,18 +9,30 @@ public class Fracture : MonoBehaviour {
 	float forceToBreak;
 	[SerializeField]
 	float fractureModelRatio; //Ratio of the Whole:Fractured size
+	[SerializeField]
+	bool requiresSupport; //Bool to check if the thing requires support
+
 
 	bool hasBeenDestroyed; //Stops double spawning
 
 
+	Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
+		rb = GetComponent<Rigidbody> ();
 		hasBeenDestroyed = false;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+		CheckForSupport ();
+	}
+
+	void CheckForSupport() {
+		if(!hasBeenDestroyed && !Physics.Raycast(transform.position, Vector3.down, 0.3f) && requiresSupport) {
+			Break ();
+		}
 	}
 
 	void OnCollisionEnter(Collision coll) {
@@ -34,20 +46,25 @@ public class Fracture : MonoBehaviour {
 				
 				Debug.Log ("Collision at " + controller.device.velocity.magnitude);
 				if(Mathf.Abs(controller.device.velocity.magnitude) > forceToBreak && !hasBeenDestroyed) {
-					hasBeenDestroyed = true;
-					Destroy (this.gameObject);
-					//Replace?
+					Break ();
 
-					GameObject thing;
-					if (transform.parent != null) {
-						thing = Instantiate (fractureObject, transform.parent.position, Quaternion.identity);
-						thing.transform.localScale = new Vector3(transform.parent.localScale.x / fractureModelRatio, transform.parent.localScale.y / fractureModelRatio, transform.parent.localScale.z / fractureModelRatio);
-					} else {
-						thing = Instantiate (fractureObject, transform.position, Quaternion.identity);
-						thing.transform.localScale = new Vector3(transform.localScale.x / fractureModelRatio, transform.localScale.y / fractureModelRatio, transform.localScale.z / fractureModelRatio);
-					}
 				}
 			}
+		}
+	}
+
+	void Break() {
+		hasBeenDestroyed = true;
+		Destroy (this.gameObject);
+		//Replace?
+
+		GameObject thing;
+		if (transform.parent != null) {
+			thing = Instantiate (fractureObject, transform.parent.position, Quaternion.identity);
+			thing.transform.localScale = new Vector3(transform.parent.localScale.x / fractureModelRatio, transform.parent.localScale.y / fractureModelRatio, transform.parent.localScale.z / fractureModelRatio);
+		} else {
+			thing = Instantiate (fractureObject, transform.position, Quaternion.identity);
+			thing.transform.localScale = new Vector3(transform.localScale.x / fractureModelRatio, transform.localScale.y / fractureModelRatio, transform.localScale.z / fractureModelRatio);
 		}
 	}
 }
