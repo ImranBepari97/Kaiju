fbf64ab4fcbbbbb7658cbe90d0ed5903bf3379d9
Imran Bepari <imranbepari97@gmail.com>
2018-05-04 12:55:47 +0100

    Movement is done

--- Assets/Scripts/ControllerScript.cs
+++ Assets/Scripts/ControllerScript.cs
@@ -5,16 +5,47 @@ using UnityEngine;
 public class ControllerScript : MonoBehaviour {
 
 	public SteamVR_TrackedObject trackedObject;
-	public SteamVR_Controller.Device device; //Device lets you access velocity
+	public SteamVR_Controller.Device device; //Device lets you access velocity and controls
+
+
+	//Movement variables
+	static ControllerScript currentMovingController; //what controllers being used to drag
+	static bool isDragging; //whether theyre moving or not
+	static Vector3 oldControllerPosition; //controller position, check distance for movement
+	static Vector3 oldPlayerPosition; //use distance to offset player
+	public float dragRatio; //drag speed ratio
 
 	// Use this for initialization
 	void Start () {
 		trackedObject = GetComponent<SteamVR_TrackedObject> ();
 		device = SteamVR_Controller.Input ((int)trackedObject.index);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	//	Debug.Log (device.velocity);
+		//On press
+		if(device.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad)) {
+			currentMovingController = this;
+			isDragging = true;
+			oldControllerPosition = transform.localPosition;
+			oldPlayerPosition = transform.parent.position;
+
+		}
+
+		//On release
+		if(device.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad)) {
+			if(currentMovingController == this) {
+				isDragging = false;
+			}
+		}
+
+		//When dragging
+		if(isDragging) {
+			Vector3 controllerDiff = currentMovingController.gameObject.transform.localPosition - oldControllerPosition; //get difference in drag distance
+			controllerDiff = new Vector3 (controllerDiff.x * dragRatio, 0 , controllerDiff.z * dragRatio); //fix it
+			Vector3 final = new Vector3(oldPlayerPosition.x - controllerDiff.x, transform.parent.position.y , oldPlayerPosition.z - controllerDiff.z); //Offset the player
+			transform.parent.position = final;
+		}
 	}
 }
